52c20b8954ab15fda50722cbd1fd90ab89f3923a
Piotr Muras <piotrekm7@gmail.com>
2020-10-04 23:43:01 +0200

    favorite movies

--- movies/omdbapi.py
+++ movies/omdbapi.py
@@ -1,7 +1,11 @@
 from typing import List
 from dataclasses import dataclass
+import inspect
 
 import requests
+
+apikey = 'b82e5b2b'
+base_url = f'http://www.omdbapi.com/?apikey={apikey}'
 
 
 @dataclass
@@ -11,6 +15,19 @@ class Movie:
     imdbID: str
     Type: str
     Poster: str
+
+
+@dataclass
+class MovieDetails(Movie):
+    Plot: str
+    Director: str
+
+    @classmethod
+    def from_dict(cls, json):
+        return cls(**{
+            k: v for k, v in json.items()
+            if k in inspect.signature(cls).parameters
+        })
 
 
 @dataclass
@@ -25,11 +42,16 @@ class SearchResults:
         if self.Response:
             self.Search = [Movie(**m) for m in kwargs['Search']]
             self.totalResults = int(kwargs['totalResults'])
-            self.pages = (self.totalResults-1)//10 + 1
+            self.pages = (self.totalResults - 1) // 10 + 1
 
 
 def search_movies(title: str = "", page: int = 1):
-    apikey = 'b82e5b2b'
-    url = f'http://www.omdbapi.com/?apikey={apikey}&s={title}&page={page}'
+    url = f'{base_url}&s={title}&page={page}'
     response = requests.get(url)
     return SearchResults(**response.json())
+
+
+def get_movie(imdbID: str):
+    url = f'{base_url}&i={imdbID}'
+    response = requests.get(url)
+    return MovieDetails.from_dict(response.json())
--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('search/', views.search_movies)
+    path('search/', views.search_movies),
+    path('favorites/', views.Favorites.as_view()),
+    path('add_to_favorites/<str:imdbID>/', views.add_to_favorites),
+    path('remove_from_favorites/<str:imdbID>/', views.remove_from_favorites)
 ]
